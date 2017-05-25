3ccf3cc50dc83cc47b6389211c328df41f41a35b
wikibady <wikibady@wikibadydeMacBook-Pro.local>
2017-05-25 16:41:29 +0800

    map 5/25 16:41

--- pages/order_detail/order_detail.js
+++ pages/order_detail/order_detail.js
@@ -3,7 +3,28 @@ var app = getApp()
 Page({
   data: {
     order_list: [],
-    orderid:0
+    orderid:0,
+    Mapmarkers:[{
+      id:0,
+      latitude:45.755936,
+      longitude:126.633310,
+      title:"回收车",
+      iconPath:"../../static/image/mapcar.png",
+      alpha:0.7,
+      width:20,
+      height:20
+    },{
+      id:1,
+      longitude:126.639458,
+      latitude:45.751602,
+      title:"我家",
+      iconPath:"../../static/image/home.png",
+      alpha:0.7,
+      width:20,
+      height:20
+    }],
+    longitude:126.639458,
+    latitude:45.751602
   },
   URL: 'http://easy-mock.com/mock/59070ef87a878d73716e3aa7/wx-irecycle/',
 
